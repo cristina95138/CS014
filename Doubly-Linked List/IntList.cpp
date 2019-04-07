b132d2bdd9364e1fed7c7b65a0e1591a84aa6b40
Cristina Lawson <lawson.cristina@gmail.com>
2019-04-06 23:11:41 -0700

    Push

---
+++ Doubly-Linked List/IntList.cpp
@@ -0,0 +1,116 @@
+//
+// Created by Cristina Lawson on 2019-04-06.
+//
+
+#include <iostream>
+using namespace std;
+
+#include "IntList.h"
+
+IntList::IntList() {
+    dummyHead = new IntNode(0);
+    dummyTail = new IntNode(0);
+
+    dummyHead->prev = nullptr;
+    dummyHead->next = dummyTail;
+
+    dummyTail->prev = dummyHead;
+    dummyTail->next = nullptr;
+}
+
+IntList::~IntList() {
+    while (!empty()) {
+        pop_front();
+    }
+    delete dummyHead;
+    delete dummyTail;
+}
+
+void IntList::push_front(int value) {
+    IntNode* temp = new IntNode(value);
+
+    if (empty()) {
+        dummyHead->next = temp;
+        temp->prev = dummyHead;
+        temp->next = dummyTail;
+        dummyTail->prev = temp;
+    }
+    else {
+        temp->next = dummyHead->next;
+        dummyHead->next->prev = temp;
+        dummyHead->next = temp;
+        temp->prev = dummyHead;
+    }
+}
+
+void IntList::pop_front() {
+    IntNode* temp = dummyHead->next;
+
+    if (empty()) {
+
+    }
+    else {
+        dummyHead->next = temp->next;
+        temp->next->prev = dummyHead;
+        delete temp;
+        temp = nullptr;
+    }
+}
+
+void IntList::push_back(int value) {
+    IntNode* temp = new IntNode(value);
+
+    if (empty()) {
+        push_front(value);
+    }
+    else {
+        temp->next = dummyTail;
+        temp->prev = dummyTail->prev;
+        dummyTail->prev = temp;
+        temp->prev->next = temp;
+    }
+}
+
+void IntList::pop_back() {
+    IntNode* prevNode = dummyTail->prev;
+
+    if (!empty()) {
+        dummyTail->prev = prevNode->prev;
+        prevNode->prev->next = dummyTail;
+        delete prevNode;
+        prevNode = nullptr;
+    }
+}
+
+bool IntList::empty() const {
+    if (dummyHead->next != dummyTail && dummyTail->prev != dummyHead) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+ostream & operator<<(ostream &out, const IntList &rhs) {
+    IntNode* i = nullptr;
+    IntNode* outNode = rhs.dummyHead;
+
+    for (i = outNode->next; i != rhs.dummyTail; i = i->next) {
+        out << i->data;
+        if (i->next != rhs.dummyTail) {
+            out << " ";
+        }
+    }
+    return out;
+}
+
+void IntList::printReverse() const {
+    IntNode* i = nullptr;
+
+    for (i = dummyTail->prev; i != dummyHead; i = i->prev) {
+        cout << i->data;
+        if (i->prev != dummyHead) {
+            cout << " ";
+        }
+    }
+}
---
+++ Doubly-Linked List/main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+#include "IntList.h"
+
+int main() {
+    cout << "Enter a test number(1-3): ";
+    int test;
+    cin >> test;
+    cout << endl;
+    //tests constructor, destructor, push_front, pop_front, display
+    if (test == 1) {
+        cout << "\nlist1 constructor called" << flush;
+        IntList list1;
+        cout << "\npushfront 10" << flush;
+        list1.push_front(10);
+        cout << "\npushfront 20" << flush;
+        list1.push_front(20);
+        cout << "\npushfront 30" << flush;
+        list1.push_front(30);
+        cout << "\nlist1: " << flush;
+        cout << list1;
+        cout << "\nlist1 reverse: " << flush;
+        list1.printReverse();
+        cout << "\npop" << flush;
+        list1.pop_front();
+        cout << "\nlist1: " << flush;
+        cout << list1;
+        cout << "\npop" << flush;
+        list1.pop_front();
+        cout << "\nlist1: " << flush;
+        cout << list1;
+        cout << "\npop" << flush;
+        list1.pop_front();
+        cout << "\nlist1: " << flush;
+        cout << list1;
+        cout << endl;
+    }
+    if (test == 1) {
+        cout << "list1 destructor called" << endl;
+    }
+
+    //tests push_back
+    if (test == 2) {
+        cout << "\nlist2 constructor called" << flush;
+        IntList list2;
+        cout << "\npushback 10" << flush;
+        list2.push_back(10);
+        cout << "\npushback 20" << flush;
+        list2.push_back(20);
+        cout << "\npushback 30" << flush;
+        list2.push_back(30);
+        cout << "\nlist2: " << flush;
+        cout << list2;
+        cout << "\nlist2 reverse: " << flush;
+        list2.printReverse();
+        cout << "\npop" << flush;
+        list2.pop_front();
+        cout << "\nlist2: " << flush;
+        cout << list2;
+        cout << "\npop" << flush;
+        list2.pop_front();
+        cout << "\nlist2: " << flush;
+        cout << list2;
+        cout << "\npop" << flush;
+        list2.pop_front();
+        cout << "\nlist2: " << flush;
+        cout << list2;
+        cout << endl;
+    }
+    if (test == 2) {
+        cout << "list2 destructor called" << endl;
+    }
+
+    //tests selection_sort
+    if (test == 3) {
+        cout << "\nlist3 constructor called" << flush;
+        IntList list3;
+        cout << "\npushfront 10" << flush;
+        list3.push_front(10);
+        cout << "\npushfront 20" << flush;
+        list3.push_front(20);
+        cout << "\npushfront 30" << flush;
+        list3.push_front(30);
+        cout << "\nlist3: " << flush;
+        cout << list3;
+        cout << "\nlist3 reverse: " << flush;
+        list3.printReverse();
+        cout << "\npop" << flush;
+        list3.pop_front();
+        cout << "\npop" << flush;
+        list3.pop_front();
+        cout << "\npop" << flush;
+        list3.pop_front();
+        cout << "\nlist3: " << flush;
+        cout << list3;
+        cout << "\npushfront 10" << flush;
+        list3.push_front(10);
+        cout << "\nlist3: " << flush;
+        cout << list3;
+        cout << "\npushfront 20" << flush;
+        list3.push_front(20);
+        cout << "\nlist3: " << flush;
+        cout << list3;
+        cout << "\nlist3 reverse: " << flush;
+        list3.printReverse();
+        cout << endl;
+    }
+    if (test == 3) {
+        cout << "list3 destructor called" << endl;
+    }
+
+
+    return 0;
+}
